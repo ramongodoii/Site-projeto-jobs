581f054be815bd4fdd5610fddfe9ac7149916158
Ramon Godoi <ramon.dt4@gmail.com>
2024-12-31 08:37:52 -0300

    teste 2.0

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('railway', 'postgres', 'FcKtaoMIUCWlUcLLDQJkbSDdsrMpwiMM', {
-  host: 'postgres.railway.internal',
+// Configuração do Sequelize com os novos argumentos
+const sequelize = new Sequelize('postgres_do_site_jobs', 'postgres_do_site_jobs_user', 'M1B98XNgYDPJeNNSsWrYuOJyP5K6ISAP', {
+  host: 'dpg-ctptbv0gph6c73dhlr60-a',
+  port: 5432,
   dialect: 'postgres',
-  logging: false,
+  logging: false, // Define como 'true' se quiser exibir os logs de consultas no console
 });
 
-sequelize.authenticate().then(() => {
-  console.log('Conectado ao banco!');
-}).catch((erro) => {
-  console.error('Erro ao conectar:', erro);
-});
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conectado ao banco!');
+  })
+  .catch((erro) => {
+    console.error('Erro ao conectar:', erro);
+  });
 
 module.exports = sequelize;
